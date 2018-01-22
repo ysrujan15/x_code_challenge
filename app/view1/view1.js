7485d204b29c9c6df418ac3a6b82e27ef7ced6ba
Srujan <srujan.y01@gmail.com>
2018-01-22 11:05:17 -0500

    Developed page 1 sorting and files formatted

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,14 +2,39 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.controller('View1Ctrl', [ '$scope','$http', function($scope,$http) {
+    .controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
 
 
-  }]);
+        $http.get('../data.json').then(function (res) {
+            console.log(res.data);
+            $scope.values = res.data;
+            // format(res.data);
+        });
+
+        $scope.place = {
+            column: 'category',
+            descending: false
+        };
+
+        $scope.sort = function (val) {
+            //$scope.sortBy = val;
+            var place = $scope.place;
+            if (place.column == val)
+                place.descending = !place.descending;
+            else {
+                place.column = val;
+                place.descending = false;
+            }
+
+            $scope.place = place;
+        };
+
+
+    }]);
